d58eeb566100c37d9bd0436bbe22f31b624d8529
vthang95 <tranvietthang95@gmail.com>
2017-04-15 12:01:05 +0700

    change css

--- day1/app.js
+++ day1/app.js
@@ -6,49 +6,48 @@ const _fs = require('./my_modules/note.js')
 
 var app = express();
 
-//set public folder public
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send('./public/index.html');
+  res.send('./public/index.html');
 });
 
 app.get('/image/add', (req, res) => {
-    let imageInfo = {
-        name: req.query.name,
-        imageLink: req.query.imageLink,
-        description: req.query.description
-    }
-
-    _fs.saveData(imageInfo);
-    res.send('Success!');
+  let imageInfo = {
+    name: req.query.name,
+    imageLink: req.query.imageLink,
+    description: req.query.description
+  }
+  let savedData = _fs.saveData(imageInfo);
+  if (savedData) res.send('Success!');
+  else res.send('This image is already exists! Please try another one!');
 });
 
 app.get('/image/get', (req, res) => {
-    let contentWillShowUp = '';
-    try {
-        let imageInfoObj = JSON.parse(fs.readFileSync('./imageData.json', 'utf8'));
-        for (let i = 0; i < imageInfoObj.length; i++) {
-            contentWillShowUp += `
-                <div>
-                    Image Name: <span id="imageName">${ imageInfoObj[i].name }</span>
-                </div>
-                <div>
-                    Image Description: <label id="imageName">${ imageInfoObj[i].description }</label>
-                </div>
-                <div>
-                    <img src="${ imageInfoObj[i].imageLink }" width="350"/>
-                </div>
-            `
-        }
-    } catch (e) {
-
+  let contentWillShowUp = '';
+  try {
+    let imageInfoObj = JSON.parse(fs.readFileSync('./imageData.json', 'utf8'));
+    for (let i = 0; i < imageInfoObj.length; i++) {
+      contentWillShowUp += `
+        <div style="float: left; margin: 2px 2px 20px 2px">
+          <div style="float:left;width: 100%;background-color: white; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"> 
+            <img src="${ imageInfoObj[i].imageLink }" style="border-radius: 8px 8px 0px 0px" width="auto" height="200">
+            <div style="text-align: center; padding: 10px 0px 1px 0px">
+              <strong>${ imageInfoObj[i].name }</strong>
+              <p>${ imageInfoObj[i].description }</p>
+            </div>
+          </div>
+        </div>
+      `;
     }
-    res.send(contentWillShowUp);
+  } catch (e) {
+
+  }
+  res.send(contentWillShowUp);
 });
 
 const server = http.createServer(app);
 
 server.listen(6969, (req, res) => {
-    console.log('App is running on 6969...');
+  console.log('App is running on 6969...');
 });
--- day1/my_modules/note.js
+++ day1/my_modules/note.js
@@ -1,29 +1,28 @@
 const fs = require('fs');
 
 const saveData = singleData => {
-    notes = [];
+  notes = [];
 
-    const test = fs.readFileSync('./imageData.json', 'utf8');
+  try {
+    let noteString = fs.readFileSync('./imageData.json');
+    notes = JSON.parse(noteString);
+  } catch (e) {}
 
-    try {
-        let noteString = fs.readFileSync('./imageData.json');
-        notes = JSON.parse(noteString);
-    } catch (e) {}
+  let duplicateNote = notes.filter(note =>
+    note.name === singleData.name &&
+    note.description === singleData.description &&
+    note.imageLink === singleData.imageLink
+  );
 
-    let duplicateNote = notes.filter(note =>
-        note.name === singleData.name &&
-        note.description === singleData.description &&
-        note.imageLink === singleData.imageLink
-    );
-
-    if (duplicateNote.length === 0) {
-        notes.push(singleData);
-        fs.writeFileSync('./imageData.json', JSON.stringify(notes));
-    } else {
-        confirm('This picture detail is exist! Please try another one ;))');
-    }
+  if (duplicateNote.length === 0) {
+    notes.push(singleData);
+    fs.writeFileSync('./imageData.json', JSON.stringify(notes));
+  } else {
+    return false;
+  }
+  return true;
 };
 
 module.exports = {
-    saveData
+  saveData
 }
