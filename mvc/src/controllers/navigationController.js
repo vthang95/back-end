a3fac7c8ac9f793e4a8dc3d3762470e738176f50
vthang95 <tranvietthang95@gmail.com>
2017-05-03 11:40:43 +0700

    add image to homepage

--- mvc/src/controllers/navigationController.js
+++ mvc/src/controllers/navigationController.js
@@ -1,8 +1,22 @@
+const Image = require('../models/ImageModel');
 
-exports.getIndex = (req, res) => {
-  res.render('home', {
-    title: 'Home',
-  });
+
+exports.getIndex = (req, res, next) => {
+  Image
+    .find()
+    .sort({ id: -1})
+    .limit(10)
+    .exec((err, docs) => {
+    if (err) {
+      console.log(err);
+      return next(err);
+    }
+    console.log(docs)
+    res.render('home', {
+      title: 'Home',
+      images: docs 
+    });
+  })
 };
 
 exports.getContact = (req, res) => {
